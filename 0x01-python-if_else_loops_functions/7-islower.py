c434707d5d397cdcce4b4645751c0a3eab5f2da7
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-13 04:25:49 -0700

    function that returns True if an input character is lowercase, False if it is not

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,14 @@
+def islower(c):
+    """
+    This function checks for a lowercase character
+
+    Args:
+        c: the character to be checked
+
+    Returns:
+        True if c is lowercase, False if not
+    """
+
+    if ord(c) in range(97, 123):
+        return True
+    return False
