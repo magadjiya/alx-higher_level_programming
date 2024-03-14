b7f9942354cb2861a2ba6b674b5b0b693c55ef2c
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-14 02:48:17 -0700

    add two numbers together

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """
+    Add two numbers
+
+    Args:
+        a: first number
+        b: second number
+
+    Return:
+        Addtion of a and b
+    """
+    return (a + b)
