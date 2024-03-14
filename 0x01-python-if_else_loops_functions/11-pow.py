e59dca6076b057bdaf30497fea338ded46b0a3c8
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-14 02:56:41 -0700

    raise a number to the power of another number

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """
+    Raises a number a to the power of b
+
+    Args:
+        a: the number
+        b: the power
+
+    Return:
+        a ^ b
+    """
+    return(a ** b)
