114f9627e8ba577912a72981b0a1979fa09214f1
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-16 12:21:29 -0700

    print integer list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Prints all the integera in a list
+
+    Args:
+        @my_list: a list containing integers
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
