5cb30fc2adc85b8a0b9f9e840f2713fbb1de98ff
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-17 14:56:58 -0700

    print a list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """
+    Prints a list in reverse
+
+    Args:
+        @my_list: the list to be printed in reverae
+    """
+    my_list.reverse()
+    for i in my_list:
+        print("{}".format(i))
