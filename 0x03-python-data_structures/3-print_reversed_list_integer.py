efeb0f341f82caa2a7237b1d47f621c668565574
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-17 15:24:12 -0700

    looping through the range instead

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,5 +8,5 @@ def print_reversed_list_integer(my_list=[]):
         @my_list: the list to be printed in reverae
     """
     my_list.reverse()
-    for i in my_list:
-        print("{}".format(i))
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
