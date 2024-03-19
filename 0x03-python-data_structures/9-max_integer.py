a315ac06b950f61ac78723c64f5fe6f272d82049
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-19 07:32:18 -0700

    negative numbers are also compared

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -13,7 +13,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
 
-    maximum = 0
+    maximum = my_list[0]
     for num in my_list:
         if num > maximum:
             maximum = num
