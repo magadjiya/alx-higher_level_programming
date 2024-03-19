639c34a484fab22c8acf0f686857907b6c2bce17
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-19 07:29:06 -0700

    Check if list is None, and return None if it is

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,6 +7,9 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         @my_list: the list to be printed in reverae
     """
+    if my_list is None:
+        return (None)
+
     my_list.reverse()
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
