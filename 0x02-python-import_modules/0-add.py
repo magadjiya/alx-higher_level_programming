14c0ad82369bcbe8b99964ae563cfc3536a7deea
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-20 15:11:53 -0700

    importing just the add function into my main script

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as addition
+from add_0 import add
 
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, addition.add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
