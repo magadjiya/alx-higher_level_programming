1b8cdb09a4620b1015036ad564fa394d25e63953
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-03-20 20:31:03 -0700

    argv and argc: command line arguments in python!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+
+    argc = len(sys.argv)
+
+    if argc == 1:
+        print("{:d} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{:d} argument:".format(argc - 1))
+        print("{:d}: {}".format(argc - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(argc - 1))
+        for arg in range(1, argc):
+            print("{:d}: {}".format(arg, sys.argv[arg]))
