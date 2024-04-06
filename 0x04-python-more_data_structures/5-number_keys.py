ce29208e6ec9ed9a3321671a9387aaf7efcb4091
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-04-06 13:46:49 -0700

    number of keys in a dictionary

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a
+    dictionary
+
+    Args:
+        @a_dictionary: The dictionary
+    """
+    return(len(list(a_dictionary)))
