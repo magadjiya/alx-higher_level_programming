39c596bbfbd4ce7a6b85fff2d537e688f1b45cb1
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-04-06 14:37:46 -0700

    coding style fixed

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
+    """
+    Gives the number of keys a dictionary has
+
+    Args:
+        @a_dictionary: the dictionary
+
+    Return:
+        The total number of keys
+    """
     return(len(list(a_dictionary)))
