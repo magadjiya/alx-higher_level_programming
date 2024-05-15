d7128e9fc08138726d61f87eeb613ccc22cf1951
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-05-15 08:41:49 -0700

    exception raised, syntax fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -14,10 +14,10 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            if type(my_list[i]) == int:
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         except IndexError:
-            break
+            raise
     print()
     return (count)
