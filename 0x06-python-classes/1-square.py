3626d7ee64e20dab4bbe4e8b34692ae24f098cf8
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-05-25 12:16:55 -0700

    tasks 0, 1 and 2 - define a class Square, with private instance size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Defines a square based on 0-square.py
+    """
+    def __init__(self, size):
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Defines a square based on 1-square.py
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
