6f6c2659f6516532df862233b53ef6698375bdd8
Khadija Umar Danhassan <khadijaumardh@gmail.com>
2024-05-28 06:19:14 -0700

    module documented

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This module contains a class that defines a square
+
+Class:
+    Square: an empty clazs
+"""
+
 class Square:
     """
     This is an empty class that defines a square
